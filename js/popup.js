475a38b98aca0d84a15a72060a3d66543beac132
RottenZulu <olliesmash@gmail.com>
2018-05-24 22:17:49 -0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,17 @@
+/*
+Jquery for Portfolio Modal Popup
+*/
 
+$(document).ready(function(e) {
+
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".open").on("click", function(){
+    $(".popup-overlay, .popup-content").addClass("active");
+  });
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked
+  $(".close, .popup-overlay").on("click", function(){
+    $(".popup-overlay, .popup-content").removeClass("active");
+  });
+
+});
